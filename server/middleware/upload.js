3bd8225a25e51e8929027e4b0d42c33a170b8e84
Roja <rojaeswari2729@gmail.com>
2026-07-15 15:16:32 +0530

    Resolved49

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,33 +1,76 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname));
+//   },
+// });
+
+// const upload = multer({
+//   storage,
+//   fileFilter: (req, file, cb) => {
+
+//     const allowed =
+//       /jpeg|jpg|png|gif|pdf|doc|docx|xls|xlsx|txt/;
+
+//     const ext = allowed.test(
+//       path.extname(file.originalname).toLowerCase()
+//     );
+
+//     if (ext) {
+//       cb(null, true);
+//     } else {
+//       cb(new Error("Only Images & Documents Allowed"));
+//     }
+//   },
+// });
+
+// module.exports = upload;
+
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
 
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "chat_uploads",
+
+    allowed_formats: [
+      "jpg",
+      "jpeg",
+      "png",
+      "gif",
+      "pdf",
+      "doc",
+      "docx",
+      "xls",
+      "xlsx",
+      "txt"
+    ],
+
+    resource_type: "auto",
   },
 });
+
 
 const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-
-    const allowed =
-      /jpeg|jpg|png|gif|pdf|doc|docx|xls|xlsx|txt/;
-
-    const ext = allowed.test(
-      path.extname(file.originalname).toLowerCase()
-    );
-
-    if (ext) {
-      cb(null, true);
-    } else {
-      cb(new Error("Only Images & Documents Allowed"));
-    }
-  },
+  storage: storage,
 });
+
 
 module.exports = upload;
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -17,14 +17,27 @@ router.put("/status/:id", updateMessageStatus);
 router.put("/seen/:id", updateSeenStatus);
 router.get("/:senderId/:receiverId", getMessages);
 
+// router.post("/upload", upload.single("image"), (req, res) => {
+//   try {
+//     res.json({
+//       message: "Image uploaded successfully",
+//       image: req.file.filename,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({
+//       message: "Upload Failed",
+//     });
+//   }
+// });
 router.post("/upload", upload.single("image"), (req, res) => {
   try {
     res.json({
       message: "Image uploaded successfully",
-      image: req.file.filename,
+      image: req.file.path,
     });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(500).json({
       message: "Upload Failed",
     });
