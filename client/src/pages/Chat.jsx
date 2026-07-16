612454583c3393fe51a8ae43fb431e7cccd15f6f
Roja <rojaeswari2729@gmail.com>
2026-07-16 15:14:42 +0530

    page13

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -423,20 +423,20 @@ function Chat() {
             status: "delivered",
           }
         );
-        socket.emit("message_delivered", {
-            id: data.id,
-            status: "delivered",
-          });
+        // socket.emit("message_delivered", {
+        //     id: data.id,
+        //     status: "delivered",
+        //   });
 
 
         // Seen
         await axios.put(
           `https://chat-box-2-hyl4.onrender.com/api/messages/seen/${data.id}`
         );
-        socket.emit("message_seen", {
-            id: data.id,
-            status: "seen",
-          });
+        // socket.emit("message_seen", {
+        //     id: data.id,
+        //     status: "seen",
+        //   });
         
 
       } catch (err) {
